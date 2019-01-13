a0b1fe4df95d4e2386af3e6210c64b0ca7c7d3cd
Marcus Bischof <marcusbischof@marcuss-mbp.attlocal.net>
2019-01-13 14:53:20 -0600

    Import to .csv done as requested, with two values (id , name) per row, no headers

--- Q1/script.py
+++ Q1/script.py
@@ -13,6 +13,9 @@ import time
 import sys
 import collections
 
+# For csv output
+import csv
+
 # If this program is called as detailed in the HW description: 'python3 script.py <API_KEY>'
 # then api_key will contain the appropriate API_KEY from TMDb.
 api_key = str(sys.argv[1])
@@ -43,12 +46,11 @@ drama_id = [val['id'] for val in genre_list_response['genres'] if val['name'] ==
 # API Constraint: Cannot make > 40 requests per 10 seconds.
 #
 ################################################
-
+page = 1
+movies = []
 # Initially 1 since we made a request above to get drama ID
-page = contiguous_requests = 1
-drama_movies_since_2004_sorted_by_popularity = []
-
-while len(drama_movies_since_2004_sorted_by_popularity) < 350:
+contiguous_requests = 1
+while len(movies) < 350:
     top_350_drama_request='/3/discover/movie?api_key={}&language=en-US&sort_by=popularity.desc&page={}&primary_release_date.gte=2004-01-01&with_genres={}'.format(api_key, page, drama_id)
     page += 1
     contiguous_requests += 1
@@ -56,13 +58,19 @@ while len(drama_movies_since_2004_sorted_by_popularity) < 350:
     response = connection.getresponse()
     top_350_single_response = json.loads(response.read().decode())
     for resp_obj in top_350_single_response['results']:
-        if len(drama_movies_since_2004_sorted_by_popularity) < 350:
-            drama_movies_since_2004_sorted_by_popularity.append((resp_obj['id'], resp_obj['original_title']))
+        if len(movies) < 350:
+            movies.append((resp_obj['id'], resp_obj['original_title']))
         else:
             break
-    # Ensures we don't overrequest
     if contiguous_requests == 40:
         sleep(10)
         contiguous_requests=0
+
+# Save values to csv
+with open('movie_ID_name.csv', 'w') as f:
+    w = csv.DictWriter(f, ['id', 'name'])
+    # We do not call w.writeheader() as per homework.
+    for row in movies:
+        w.writerow({'id' : row[0],'name' : row[1]})
 
 connection.close()
